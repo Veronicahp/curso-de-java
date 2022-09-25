5e2220fd1f95bd9b2aa7402f747a92e92531a010
Verônica Paranhos <veronicahp@gmail.com>
2022-09-24 23:07:54 -0300

    git commit -m "Atualizando repo"

--- Heranca/src/Heranca.java
+++ Heranca/src/Heranca.java
@@ -3,8 +3,18 @@ public class Heranca {
 
         Vendedor v = new Vendedor();
         v.setNome("Verônica");
-        v.setSalario(100.0f);
+        v.setSalario(1000.0f);
+        v.setCpf("90090090090");
+        v.setDataNascimento(new Date());
+        v.setComissaoItem(10.0f);
+        v.setTotalItensVendidos(10);
+
+        System.out.println("O salário do vendedor é: " + v.calcularSalario());
 
         Motorista m = new Motorista();
+        m.setNome("Marcio");
+        m.setCpf("90090090091");
+        m.setDataNascimento(new Date());
+        m.setSalario(salario:500.0f);
     }
 }
--- Heranca/src/Vendedor.java
+++ Heranca/src/Vendedor.java
@@ -13,6 +13,23 @@ public class Vendedor  extends Funcionario {
    public float calcularSalario() {
     return super.getSalario() + (this.comissaoItem * totalItensVendidos)
    }
+
+    public int getTotalItensVendidos() {
+        return totalItensVendidos;
+    }
+
+    public void setTotalItensVendidos(int totalItensVendidos) {
+        this.totalItensVendidos = totalItensVendidos;
+    }
+
+    public float getComissaoItem() {
+        return comissaoItem;
+    }
+
+    public void setComissaoItem(float comissaoItem) {
+        this.comissaoItem = comissaoItem;
+    }
+
 }
 
 
